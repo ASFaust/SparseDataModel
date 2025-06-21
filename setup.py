1b6d0361b1af8388ff3aa25772a9f5c24f937931
ASFaust <andreas.s.faust@gmail.com>
2025-06-21 19:06:25 +0200

    small fix

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sparse_data_modeling",               # package name on PyPI (if you publish)
+    name="sparse_data_modeling",
     version="1.0.0",
     description="Sparse data covariance model",
     author="Andreas Faust",
     author_email="andreas.s.faust@gmail.com",
-    packages=find_packages(),        # finds 'sparse_cov'
+    packages=find_packages(),
     install_requires=[
         "numpy",
         "scipy",
         "torch"
     ],
-    python_requires=">=3.8",         # adjust as needed
+    python_requires=">=3.8",
+    license="MIT",
 )
